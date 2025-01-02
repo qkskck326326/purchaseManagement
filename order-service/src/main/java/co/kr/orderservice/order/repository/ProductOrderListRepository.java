2db3cf6bdde464c4a9d29c8dea040103e78d1b87
이건우 <qkskck3@gmail.com>
2025-01-02 18:38:07 +0900

    엔티티 이름 변경에 따른 수정

--- order-service/src/main/java/co/kr/orderservice/order/repository/ProductOrderListRepository.java
+++ order-service/src/main/java/co/kr/orderservice/order/repository/ProductOrderListRepository.java
@@ -1,13 +1,13 @@
 package co.kr.orderservice.order.repository;
 
 
-import co.kr.orderservice.order.entity.ProductOrderListEntity;
+import co.kr.orderservice.order.entity.ProductOrderItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductOrderListRepository extends JpaRepository<ProductOrderListEntity, Long> {
-    List<ProductOrderListEntity> findAllByOrderId(Long orderId);
+public interface ProductOrderListRepository extends JpaRepository<ProductOrderItemEntity, Long> {
+    List<ProductOrderItemEntity> findAllByOrderId(Long orderId);
 }
