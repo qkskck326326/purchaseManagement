54bf462ece6f3e2b46ac5a918b0efe744faeb8f1
이건우 <qkskck3@gmail.com>
2025-01-02 18:38:53 +0900

    Product 로의 요청메세지 로직 이용, 기존의 메소드 재구성

--- order-service/src/main/java/co/kr/orderservice/order/service/OrderService.java
+++ order-service/src/main/java/co/kr/orderservice/order/service/OrderService.java
@@ -2,6 +2,7 @@ package co.kr.orderservice.order.service;
 
 import co.kr.orderservice.feign.toProduct.ProductClient;
 import co.kr.orderservice.feign.toProduct.ProductResponseDto;
+import co.kr.orderservice.kafka.toProduct.ProductProducer;
 import co.kr.orderservice.order.entity.*;
 
 import co.kr.orderservice.order.repository.ProductOrderListRepository;
@@ -10,8 +11,14 @@ import co.kr.orderservice.order.repository.WishListRepository;
 import co.kr.orderservice.order.util.JwtTokenUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -21,6 +28,7 @@ public class OrderService {
     private final ProductOrderRepository productOrderRepository;
     private final ProductOrderListRepository productOrderListRepository;
     private final JwtTokenUtil jwtTokenUtil;
+    private final ProductProducer productProducer;
 
     // 장바구니 리스트 가져오기
     public List<WishListResponseDto> getWishList(String userEmail) {
@@ -59,11 +67,12 @@ public class OrderService {
     
     // 장바구니에서 상품 삭제
     public String deleteQuantityWishList(Long wishListId) {
-        // 존재 확인
-        if(wishListRepository.existsById(wishListId)) {
-            return "위시리스트에 해당 상품이 없습니다.";
-        }
         try {
+            // 존재 확인
+            if(wishListRepository.existsById(wishListId)) {
+                return "위시리스트에 해당 상품이 없습니다.";
+            }
+            // 삭제
             wishListRepository.deleteById(wishListId);
         } catch (Exception e) {
             return "삭제중 오류가 발생하였습니다.";
@@ -71,129 +80,116 @@ public class OrderService {
         return "삭제가 완료되었습니다.";
     }
 
-//    // 상품 주문 -- 편집점 -- 결제 관련 수정 필요
-//    @Transactional
-//    public String orderProducts(List<OrderListRequestDto> orderListRequestDto, String bearerToken) {
-//        String token = bearerToken.substring(7);
-//        String userEmail = jwtTokenUtil.getUserEmailFromToken(token);
-//        try {
-//            // 재고 확인 및 감소
-//            for (OrderListRequestDto orderList : orderListRequestDto) {
-//                checkQuantityOrThrow(orderList);
-//            }
-//
-//            // 주문서 저장
-//            ProductOrderEntity order = new ProductOrderEntity(userEmail);
-//            productOrderRepository.save(order);
-//
-//            // 주문 리스트로 변환 및 저장
-//            List<ProductOrderListEntity> orderList = orderListRequestDto.stream()
-//                    .map(dto -> new ProductOrderListEntity(order.getOrderId(), dto))
-//                    .toList();
-//            productOrderListRepository.saveAll(orderList);
-//
-//            return "주문이 완료되었습니다.";
-//        } catch (RuntimeException e) {
-//            return "주문 진행 중 오류: " + e.getMessage();
-//        }
-//    }
-//
-//
-//    // 재고 확인 및 감소 메소드
-//    public void checkQuantityOrThrow(OrderListRequestDto orderListRequestDto) {
-//        Optional<ProductEntity> productO = productRepository.findById(orderListRequestDto.getProductId());
-//        ProductEntity product = productO.orElseThrow(() ->
-//                new RuntimeException(orderListRequestDto.getProductName() + " 이 현재 존재하지 않습니다."));
-//
-//        if (product.getProductQuantity() >= orderListRequestDto.getQuantity()) {
-//            product.setProductQuantity(product.getProductQuantity() - orderListRequestDto.getQuantity());
-//            productRepository.save(product);
-//        } else {
-//            throw new RuntimeException(orderListRequestDto.getProductName() + " 의 재고가 부족합니다.");
-//        }
-//    }
-//
-//
-//    // 주문 취소
-//    @Transactional
-//    public String OrderCancellation(Long orderId, String bearerToken) {
-//        String token = bearerToken.substring(7);
-//        String userEmail = jwtTokenUtil.getUserEmailFromToken(token);
-//        Optional<ProductOrderEntity> orderO = productOrderRepository.findById(orderId);
-//        ProductOrderEntity order;
-//        if (orderO.isPresent()) {
-//            order = orderO.get();
-//        }else {
-//            return "주문취소 오류 : 주문번호 오류";
-//        }
-//
-//        // 주문서의 유저 이름 꺼내기
-//        String orderUser = order.getUserEmail();
-//
-//        // 주문한 유저와 신청 유저의 email 이 다르다면
-//        if (orderUser.equals(userEmail)) {
-//            return "주문취소 오류 : 잘못된 접근";
-//        }
-//
-//        if (order.getOrderState() != OrderStateEnum.Order_Completed){
-//            return "주문취소 오류 : 이미 배송된 상품은 주문취소가 불가합니다.";
-//        }
-//
-//        // 재고처리
-//        List<ProductOrderListEntity> orderLists = productOrderListRepository.findAllByOrderId(orderId);
-//        for (ProductOrderListEntity orderList : orderLists) {
-//            Optional<ProductEntity> productO = productRepository.findById(orderList.getProductId());
-//            ProductEntity product;
-//            if (productO.isPresent()) {
-//                product = productO.get();
-//                product.setProductQuantity(product.getProductQuantity() - orderList.getQuantity());
-//                productRepository.save(product);
-//            }
-//        }
-//
-//        order.setOrderState(OrderStateEnum.Order_Cancellation);
-//        productOrderRepository.save(order);
-//        return "주문이 취소되었습니다.";
-//    }
-//
-//    // 반품 신청
-//    public String orderRefund(Long orderId, String bearerToken) {
-//        String token = bearerToken.substring(7);
-//        String userEmail = jwtTokenUtil.getUserEmailFromToken(token);
-//        Optional<ProductOrderEntity> orderO = productOrderRepository.findById(orderId);
-//        ProductOrderEntity order;
-//        if (orderO.isPresent()) {
-//            order = orderO.get();
-//        }else {
-//            return "반품신청 오류 : 주문 번호 오류";
-//        }
-//
-//        // 토큰의 유저 이름 꺼내기
-//        String orderUser = jwtTokenUtil.getUserEmailFromToken(token);
-//
-//        // 주문한 유저와 신청 유저의 email 이 다르다면
-//        if (orderUser.equals(userEmail)) {
-//            return "반품신청 오류 : 잘못된 접근";
-//        }
-//
-//        // 상품을 받은지 1일 이상이 지났다면
-//
-//        if (order.getOrderState() != OrderStateEnum.Delivery_Completed){
-//            return "반품신청 오류 : 배송완료 되지 않은 상품은 반품이 불가능합니다.";
-//        }
-//
-//        // 시간계산
-//        LocalDateTime limitTime = order.getUpdateAt()
-//                .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
-//                .plusDays(1);   // LocalDateTime 으로 변환 및 배송받은 날짜로부터 1일 추가
-//        if (limitTime.isAfter(LocalDateTime.now())) {
-//            return "반품신청 오류 : 배송완료 후 만 1일이 지난 상품은 환불이 불가능합니다.";
-//        }
-//
-//        // 주문의 Status 변환
-//        order.setOrderState(OrderStateEnum.Refunding);
-//        order.setRefundAt(new Date());
-//        productOrderRepository.save(order);
-//        return "반품신청이 완료되었습니다.";
-//    }
+    // 상품 주문서 만들기
+    public String orderProducts(List<OrderItemRequestDto> orderListRequestDto, String bearerToken) {
+        try {
+            String token = bearerToken.substring(7); // 토큰 정제
+            String userEmail = jwtTokenUtil.getUserEmailFromToken(token); // 토큰에서 이메일 꺼내기
+
+            ProductOrderEntity order = new ProductOrderEntity(userEmail); // 꺼낸 이메일로 주문서 만들기
+            productOrderRepository.save(order); // 주문서 저장
+
+            // 재고처리 및 확인 결과 반환
+            String result = productProducer.decreaseQuantity(order.getOrderId(), orderListRequestDto);
+            System.out.println("kafka 메세지 reply 결과" + result);
+            if (!result.equals("null")){
+                return result;
+            }else {
+                // 주문 리스트로 변환 및 저장
+                List<ProductOrderItemEntity> orderList = orderListRequestDto
+                        .stream().map(dto -> new ProductOrderItemEntity(order.getOrderId(), dto))
+                        .toList();
+                System.out.println("주문 갯수 : " + orderList.size());
+
+                productOrderListRepository.saveAll(orderList);
+
+                return "주문 완료";
+            }
+        }catch (Exception e) {
+            System.out.println("주문 처리중 에러 : " + e.getMessage());
+            return "주문 처리중 에러 발생 : " + e.getMessage();
+        }
+    }
+
+    // 주문 취소
+    @Transactional
+    public String OrderCancellation(Long orderId, String bearerToken) {
+        try {
+            String token = bearerToken.substring(7);
+            String userEmail = jwtTokenUtil.getUserEmailFromToken(token);
+            Optional<ProductOrderEntity> orderO = productOrderRepository.findById(orderId);
+            ProductOrderEntity order;
+            if (orderO.isPresent()) {
+                order = orderO.get();
+            }else {
+                return "주문취소 오류 : 주문번호 오류";
+            }
+
+            // 주문서의 유저 이름 꺼내기
+            String orderUser = order.getUserEmail();
+
+            // 주문한 유저와 신청 유저의 email 이 다르다면
+            if (orderUser.equals(userEmail)) {
+                return "주문취소 오류 : 잘못된 접근";
+            }
+
+            if (order.getOrderState() != OrderStateEnum.Order_Completed){
+                return "주문취소 오류 : 이미 배송된 상품은 주문취소가 불가합니다.";
+            }
+
+            List<OrderItemRequestDto> orderItemList = productOrderListRepository.findAllByOrderId(orderId)
+                    .stream().map(entity -> new OrderItemRequestDto(entity))
+                    .toList();
+
+            // 재고처리 요청
+            productProducer.increaseQuantity(orderId, orderItemList);
+
+            order.setOrderState(OrderStateEnum.Order_Cancellation);
+            productOrderRepository.save(order);
+            return "주문이 취소되었습니다.";
+        } catch (Exception e) {
+            return "주문 취소 오류 : " + e.getMessage();
+        }
+    }
+
+    // 반품 신청
+    public String orderRefund(Long orderId, String bearerToken) {
+        String token = bearerToken.substring(7);
+        String userEmail = jwtTokenUtil.getUserEmailFromToken(token);
+        Optional<ProductOrderEntity> orderO = productOrderRepository.findById(orderId);
+        ProductOrderEntity order;
+        if (orderO.isPresent()) {
+            order = orderO.get();
+        }else {
+            return "반품신청 오류 : 주문 번호 오류";
+        }
+
+        // 토큰의 유저 이름 꺼내기
+        String orderUser = jwtTokenUtil.getUserEmailFromToken(token);
+
+        // 주문한 유저와 신청 유저의 email 이 다르다면
+        if (orderUser.equals(userEmail)) {
+            return "반품신청 오류 : 잘못된 접근";
+        }
+
+        // 상품을 받은지 1일 이상이 지났다면
+
+        if (order.getOrderState() != OrderStateEnum.Delivery_Completed){
+            return "반품신청 오류 : 배송완료 되지 않은 상품은 반품이 불가능합니다.";
+        }
+
+        // 시간계산
+        LocalDateTime limitTime = order.getUpdateAt()
+                .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+                .plusDays(1);   // LocalDateTime 으로 변환 및 배송받은 날짜로부터 1일 추가
+        if (limitTime.isAfter(LocalDateTime.now())) {
+            return "반품신청 오류 : 배송완료 후 만 1일이 지난 상품은 환불이 불가능합니다.";
+        }
+
+        // 주문의 Status 변환
+        order.setOrderState(OrderStateEnum.Refunding);
+        order.setRefundAt(new Date());
+        productOrderRepository.save(order);
+        return "반품신청이 완료되었습니다.";
+    }
 }
